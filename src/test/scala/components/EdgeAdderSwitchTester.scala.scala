9f812a6d083af771c29a6cbfd5a7e675fa1f50d4
masfiyan <asfiyanshivani0321@gmail.com>
2023-06-12 18:08:25 +0500

    Bug Fix and Add tb

---
+++ src/test/scala/components/EdgeAdderSwitchTester.scala.scala
@@ -0,0 +1,27 @@
+package magmasi.components
+
+import chisel3._
+import chisel3.util._
+import chiseltest._ 
+import org.scalatest.freespec.AnyFreeSpec
+
+class EdgeAdderSwitchTester extends AnyFreeSpec with ChiselScalatestTester {
+    "Edge Adder Switch Test" in {
+        implicit val config = MagmasiConfig()
+        test(new EdgeAdderSwitch).withAnnotations(Seq(VerilatorBackendAnnotation)) { dut =>
+            dut.io.i_data_bus(0).poke(2.U)
+            dut.io.i_data_bus(1).poke(5.U)
+            dut.io.i_data_bus(2).poke(4.U)
+            dut.io.i_data_bus(3).poke(6.U)
+            dut.io.i_valid.poke(1.B)
+            dut.io.i_add_en.poke(1.B)
+            dut.io.i_cmd.poke(5.U)
+            dut.io.i_sel.poke(1.U)
+            dut.clock.step(3)
+            //dut.io.o_adder.expect(10.U)
+
+        }
+    }
+}
+
+
