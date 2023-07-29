3a22a95f3a81dc7e52c3cfab4ce5da8b7dacd638
masfiyan <asfiyanshivani0321@gmail.com>
2023-07-29 21:47:24 +0500

    Remove bug

--- src/test/scala/components/FanNetworkTester.scala.scala
+++ src/test/scala/components/FanNetworkTester.scala.scala
@@ -8,13 +8,13 @@ import org.scalatest.freespec.AnyFreeSpec
 class FanNetworkTester extends AnyFreeSpec with ChiselScalatestTester {
     "Fan Network Test" in {
         implicit val config = MagmasiConfig()
-        test(new FanNetwork1).withAnnotations(Seq(VerilatorBackendAnnotation)) { dut =>
-            dut.io.i_valid.poke(1.B)
-            dut.io.i_data_bus.poke(2123.U)
-            dut.io.i_add_en_bus.poke(1.U)
-            dut.io.i_cmd_bus.poke(1.U)
-            dut.io.i_sel_bus.poke(1.U)
-            dut.clock.step(2)
+        test(new FanNetwork).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
+      c.io.i_valid.poke(true.B)
+      c.io.i_data_bus.poke(421111111.U)
+      c.io.i_add_en_bus.poke(10.U)
+      c.io.i_cmd_bus.poke(3.U)
+      c.io.i_sel_bus.poke(5.U)
+            c.clock.step(200)
             //dut.io.o_adder.expect(10.U)
 
         }
