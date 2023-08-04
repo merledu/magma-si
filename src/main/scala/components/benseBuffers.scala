e5041f63d2ade3a8ea404bcd5b38a81c93a0a8eb
masfiyan <asfiyanshivani0321@gmail.com>
2023-08-04 21:44:57 +0500

    benes buffers multiplication

---
+++ src/main/scala/components/benseBuffers.scala
@@ -0,0 +1,23 @@
+package  magmasi.components
+
+import chisel3._
+import chisel3.util._
+
+class benseBuffers(DATA_TYPE:Int,NUM_PES:Int) extends Module{
+      val LEVELS   : Int = (2 * (math.log(NUM_PES) / math.log(2))).toInt + 1
+
+    val io = IO(new Bundle{
+        val i_data_bus2 = Input(Vec(NUM_PES, UInt(DATA_TYPE.W)))
+        val i_data_bus1  = Input(Vec(NUM_PES, UInt(DATA_TYPE.W)))
+        val i_mux_bus   = Input(Vec(NUM_PES, UInt((LEVELS-1).W)))
+        val out = Output(Vec(NUM_PES, UInt(DATA_TYPE.W)))
+    })
+    val benes = Module(new Mybenes(8,4))
+    benes.io.i_data_bus1 <> io.i_data_bus1
+    benes.io.i_data_bus2 <> io.i_data_bus2
+    benes.io.i_mux_bus <> io.i_mux_bus
+    val buffer = Module(new buffer_multiplication(4,8))
+    buffer.io.buffer1 <> benes.io.o_dist_bus1
+    buffer.io.buffer2 <> benes.io.o_dist_bus2
+    io.out := buffer.io.out
+}
---
+++ src/test/scala/components/benesBuffersTester.scala
@@ -0,0 +1,39 @@
+
+package magmasi.components
+
+import chisel3._
+import chisel3.util._
+import chiseltest._ 
+import org.scalatest.freespec.AnyFreeSpec
+
+class benesBuffersTester extends AnyFreeSpec with ChiselScalatestTester {
+    "My Benes Top test" in {
+        implicit val config = MagmasiConfig()
+        test(new benseBuffers(8,4)).withAnnotations(Seq(VerilatorBackendAnnotation)) { dut =>
+
+            
+            val inputData = Seq(1, 2, 3, 4, 5, 6, 7, 8)
+
+      // Connect input data to the DUT
+      for (i <- 0 until 4) {
+        dut.io.i_data_bus1(i).poke(inputData(i).U)
+      }
+
+
+        dut.io.i_mux_bus(0).poke(3.U)
+        dut.io.i_mux_bus(1).poke(3.U)
+        dut.io.i_mux_bus(2).poke(3.U)
+        dut.io.i_mux_bus(3).poke(3.U)
+        val LEVELS   : Int = (2 * (math.log(4) / math.log(2))).toInt + 1
+
+
+
+
+
+      dut.clock.step(3)
+
+
+
+    }
+  }
+}
