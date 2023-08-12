547ccadf57801059e56a4f3118eb477f0159431e
Muhammad Asfiyan <asfiyanshivani0321@gmail.com>
2023-08-13 02:27:35 +0500

    All test cases are passed

--- src/test/scala/components/MybenesTester.scala
+++ src/test/scala/components/MybenesTester.scala
@@ -9,17 +9,18 @@ import org.scalatest.freespec.AnyFreeSpec
 class MybenesTester extends AnyFreeSpec with ChiselScalatestTester {
     "Benes Test" in {
         implicit val config = MagmasiConfig()
-        test(new Mybenes(8,4)).withAnnotations(Seq(VerilatorBackendAnnotation)) { dut =>
-          val inputData = Seq(1, 2, 2,4)
+        test(new MyBenes2(8,4)).withAnnotations(Seq(VerilatorBackendAnnotation)) { dut =>
+          val inputData = Seq(1,2,2,4)
           for (i <- 0 until 4) {
-            dut.io.i_data_bus1(i).poke(inputData(i).U)
+            dut.io.i_data_bus2(i).poke(inputData(i).U)
+            //dut.io.i_data_bus2(i).poke(inputData(i).U)
           }
-          dut.io.i_mux_bus(0).poke("b0000".U)
-          dut.io.i_mux_bus(1).poke("b0000".U)
-          dut.io.i_mux_bus(2).poke("b0000".U)
-          dut.io.i_mux_bus(3).poke("b0000".U)
+          dut.io.i_mux_bus(0).poke("b1000".U)
+          dut.io.i_mux_bus(1).poke("b1000".U)
+          dut.io.i_mux_bus(2).poke("b0100".U)
+          dut.io.i_mux_bus(3).poke("b1000".U)
           val LEVELS   : Int = (2 * (math.log(4) / math.log(2))).toInt + 1
-          dut.clock.step(3)
+          dut.clock.step(5)
         }
     } 
 }
