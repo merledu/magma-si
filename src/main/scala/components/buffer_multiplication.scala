b4809cefabc4340c462993b40cc1204d28551b6f
Muhammad Asfiyan <asfiyanshivani0321@gmail.com>
2023-09-07 01:17:44 +0500

    Update buffer_multiplication.scala

--- src/main/scala/components/buffer_multiplication.scala
+++ src/main/scala/components/buffer_multiplication.scala
@@ -9,6 +9,12 @@ class buffer_multiplication(NUM_PES:Int,DATA_TYPE:Int) extends Module{
         val buffer2 = Input(Vec(NUM_PES, UInt(DATA_TYPE.W)))
         val out = Output(Vec(NUM_PES, UInt(DATA_TYPE.W)))
     })
+    val array1 = WireInit(VecInit(Seq.fill(NUM_PES)(0.U(DATA_TYPE.W))))
+    val array2 = WireInit(VecInit(Seq.fill(NUM_PES)(0.U(DATA_TYPE.W))))
+    for (i <- 0 until NUM_PES){
+        array1(i) := io.buffer1(i)
+        array2(i) := io.buffer2(i)
+    }
 
     def multiply(vec1: Vec[UInt], vec2: Vec[UInt], index: Int = 0): Vec[UInt] = {
         if (index >= NUM_PES) {
@@ -21,5 +27,5 @@ class buffer_multiplication(NUM_PES:Int,DATA_TYPE:Int) extends Module{
     }
   }
 
-  io.out := multiply(io.buffer1, io.buffer2)
+  io.out := multiply(array1, array2)
 }
