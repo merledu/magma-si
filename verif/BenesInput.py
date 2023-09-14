273f6c7de5c27de4ffc911a08e07c16fba142f6a
shahzaibk23 <shahzaibceo@gmail.com>
2023-09-14 22:26:42 +0500

    minor bugs fixed

--- verif/BenesInput.py
+++ verif/BenesInput.py
@@ -12,8 +12,13 @@ def BenesInput():
 
     for key in list(MUXES_STRCT.keys()):
         inputs = MUXES_STRCT[key]
-        values = PARSED_MUXES[key]
-
+        if key in list(PARSED_MUXES.keys()):
+            values = PARSED_MUXES[key]
+        else:
+            if type(inputs) != int:
+                values = [0,0]
+            else:
+                values = 0
         if type(inputs) != int:
             strs.append(fmt.format(inputs[0], values[0]))
             strs.append(fmt.format(inputs[1], values[1]))
@@ -21,6 +26,7 @@ def BenesInput():
         else:
             strs.append(fmt.format(inputs, values))
             strs.append("\n")
+                
     
     f = open("BenesIputs.scala", "w+")
     f.write("\n".join(strs))
