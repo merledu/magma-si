8d4e5d899cc058b573ffc12d87eae793720261cb
masfiyan <asfiyanshivani0321@gmail.com>
2023-10-02 20:05:10 +0500

    AdderSwitch support for implicit parameters

--- src/main/scala/components/AdderSwitch.scala
+++ src/main/scala/components/AdderSwitch.scala
@@ -3,22 +3,22 @@ package magmasi.components
 import chisel3._
 import chisel3.util._
 
-class AdderSwitch(DATA_TYPE: Int = 32, NUM_IN: Int = 2, SEL_IN: Int = 2,NUM_OUT: Int =2) extends Module {
+class AdderSwitch(implicit val config: MagmasiConfig) extends Module {
   val io = IO(new Bundle {
     val i_valid = Input(Bool())
-    val i_data_bus = Input(Vec(NUM_IN, UInt(DATA_TYPE.W)))
+    val i_data_bus = Input(Vec(config.NUM_IN, UInt(config.DATA_TYPE.W)))
     val i_add_en = Input(UInt(3.W))
     val i_cmd = Input(UInt(3.W))
-    val i_sel = Input(UInt(SEL_IN.W))
-    val o_vn_valid = Output(UInt(NUM_OUT.W))
-    val o_vn = Output(UInt((2 * DATA_TYPE).W))
-    val o_adder = Output(Vec(NUM_OUT, UInt(DATA_TYPE.W)))
+    val i_sel = Input(UInt(config.SEL_IN.W))
+    val o_vn_valid = Output(UInt(config.NUM_OUT.W))
+    val o_vn = Output(UInt((2 * config.DATA_TYPE).W))
+    val o_adder = Output(Vec(config.NUM_OUT, UInt(config.DATA_TYPE.W)))
   })
 
   val clk = clock
   val rst = reset.asBool
 
-  val reductionMux = Module(new ReductionMux(W = DATA_TYPE, NUM_IN = NUM_IN, SEL_IN = SEL_IN))
+  val reductionMux = Module(new ReductionMux(W = config.DATA_TYPE, NUM_IN = config.NUM_IN, SEL_IN = config.SEL_IN))
   reductionMux.io.i_data := io.i_data_bus
   reductionMux.io.i_sel := io.i_sel
 
@@ -26,8 +26,8 @@ class AdderSwitch(DATA_TYPE: Int = 32, NUM_IN: Int = 2, SEL_IN: Int = 2,NUM_OUT:
   adder32.io.A := reductionMux.io.o_data(1)
   adder32.io.B := reductionMux.io.o_data(0)
 
-  val r_adder = RegInit(0.U((2 * DATA_TYPE).W))
-  val r_vn = RegInit(0.U((2 * DATA_TYPE).W))
+  val r_adder = RegInit(0.U((2 * config.DATA_TYPE).W))
+  val r_vn = RegInit(0.U((2 * config.DATA_TYPE).W))
   val r_vn_valid = RegInit(0.U(2.W))
   val r_add_en = RegNext(io.i_add_en, init = 0.U)
 
@@ -42,12 +42,12 @@ class AdderSwitch(DATA_TYPE: Int = 32, NUM_IN: Int = 2, SEL_IN: Int = 2,NUM_OUT:
         r_vn_valid := 0.U
       }
       is ("b011".U) {
-        r_adder := Cat(0.U((DATA_TYPE - 1).W), reductionMux.io.o_data(1))
+        r_adder := Cat(0.U((config.DATA_TYPE - 1).W), reductionMux.io.o_data(1))
         r_vn := io.i_data_bus(0)
         r_vn_valid := 1.U
       }
       is ("b100".U) {
-        r_adder := Cat(reductionMux.io.o_data(0), 0.U((DATA_TYPE - 1).W))
+        r_adder := Cat(reductionMux.io.o_data(0), 0.U((config.DATA_TYPE - 1).W))
         r_vn := io.i_data_bus(1)
         r_vn_valid := 2.U
       }
@@ -62,12 +62,12 @@ class AdderSwitch(DATA_TYPE: Int = 32, NUM_IN: Int = 2, SEL_IN: Int = 2,NUM_OUT:
   }
 
   when (rst) {
-    io.o_adder := VecInit(Seq.fill(2)(0.U(DATA_TYPE.W)))
+    io.o_adder := VecInit(Seq.fill(2)(0.U(config.DATA_TYPE.W)))
     io.o_vn := 0.U
     io.o_vn_valid := 0.U
   } .otherwise {
     when (!r_add_en) {
-      io.o_adder := VecInit(Seq(r_adder(DATA_TYPE - 1, 0), r_adder(2 * DATA_TYPE - 1, DATA_TYPE)))
+      io.o_adder := VecInit(Seq(r_adder(config.DATA_TYPE - 1, 0), r_adder(2 * config.DATA_TYPE - 1, config.DATA_TYPE)))
     } .otherwise {
       io.o_adder := VecInit(Seq.fill(2)(adder32.io.O))
     }
--- src/main/scala/components/MagmasiConfig.scala
+++ src/main/scala/components/MagmasiConfig.scala
@@ -3,7 +3,9 @@ import scala.math
 
 case class MagmasiConfig (
     val DATA_TYPE: Int = 16,
-    val NUM_PES  : Int = 4
+    val NUM_IN  : Int = 4,
+    val SEL_IN: Int = 2,
+    val NUM_OUT: Int =2
 ) {
-    val LEVELS   : Int = (2 * (math.log(NUM_PES) / math.log(2))).toInt + 1
+    val LEVELS   : Int = (2 * (math.log(NUM_IN) / math.log(2))).toInt + 1
 }
