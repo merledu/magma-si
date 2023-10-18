5740b13b21d75f15801ed263ba99b1ab558f6eda
Muhammad Asfiyan <asfiyanshivani0321@gmail.com>
2023-10-19 00:48:23 +0500

    Resolve Muxes

--- src/main/scala/components/Muxes.scala
+++ src/main/scala/components/Muxes.scala
@@ -7,6 +7,8 @@ class Muxes(implicit val config: MagmasiConfig) extends Module{
     val io = IO(new Bundle{
         val mat1 =Input(Vec(config.MaxRows, Vec(config.MaxCols, UInt(config.DATA_TYPE.W))))
         val mat2 = Input(Vec(config.MaxRows, UInt(config.DATA_TYPE.W)))
+        val counterMatrix1 = Input(Vec(config.MaxRows, Vec(config.MaxCols, UInt(config.DATA_TYPE.W))))
+        val counterMatrix2 = Input(Vec(config.MaxRows, UInt(config.DATA_TYPE.W)))
         val i_mux_bus = Output(Vec(config.MaxRows * config.MaxCols, UInt((config.LEVELS - 1).W)))        
         val Source  = Output(Vec(config.MaxRows * config.MaxCols, UInt(config.DATA_TYPE.W)))
         val destination  = Output(Vec(config.MaxRows * config.MaxCols, UInt(config.DATA_TYPE.W)))
@@ -27,15 +29,15 @@ class Muxes(implicit val config: MagmasiConfig) extends Module{
     dontTouch(src)
     dontTouch(dest)
 
-    when ((io.mat1(j)(i) =/= 0.U) && (io.mat2(k) =/= 0.U)) {
+    when ((io.counterMatrix1(j)(i) =/= 0.U) && (io.counterMatrix2(k) =/= 0.U)) {
       when (srcValid(counter) === 0.B){
-        when(io.mat1(j)(i) < io.mat2(k)){
-          mux(counter) :=   (io.mat2(k) - 1.U) - (io.mat1(j)(i) - 1.U)
+        when(io.counterMatrix1(j)(i) < io.counterMatrix2(k)){
+          mux(counter) :=   (io.counterMatrix2(k) - 1.U) - (io.counterMatrix1(j)(i) - 1.U)
           src(counter) := io.mat2(k)
           srcValid(counter) := 1.B
           dest(counter) := io.mat1(j)(i)
         }.otherwise{
-          mux(counter) :=   (io.mat1(j)(i) - 1.U) - (io.mat2(k) - 1.U)
+          mux(counter) :=   (io.counterMatrix1(j)(i) - 1.U) - (io.counterMatrix2(k) - 1.U)
           src(counter) := io.mat2(k)
           srcValid(counter) := 1.B
           dest(counter) := io.mat1(j)(i)
