7e536a53e600f167f7da2e4bb79f8d8d13c33338
Muhammad Asfiyan <asfiyanshivani0321@gmail.com>
2023-10-18 23:05:49 +0500

    map Bitmap as configure parameter

--- src/main/scala/components/Bitmap.scala
+++ src/main/scala/components/Bitmap.scala
@@ -5,21 +5,21 @@ import chisel3.util._
 
 class Bitmap(implicit val config: MagmasiConfig) extends Module{
     val io = IO(new Bundle {
-        val mat1 =Input(Vec(config.rowsA, Vec(config.colsA, UInt(config.DATA_TYPE.W))))
-        val mat2 = Input(Vec(config.rowsB, Vec(config.colsB, UInt(config.DATA_TYPE.W))))
-         val bitmap1 =Output(Vec(config.rowsA, Vec(config.colsA, UInt(config.DATA_TYPE.W))))
-        val bitmap2 = Output(Vec(config.rowsB, Vec(config.colsB, UInt(config.DATA_TYPE.W))))
+        val mat1 =Input(Vec(config.MaxRows, Vec(config.MaxCols, UInt(config.DATA_TYPE.W))))
+        val mat2 = Input(Vec(config.MaxRows, Vec(config.MaxCols, UInt(config.DATA_TYPE.W))))
+         val bitmap1 =Output(Vec(config.MaxRows, Vec(config.MaxCols, UInt(config.DATA_TYPE.W))))
+        val bitmap2 = Output(Vec(config.MaxRows, Vec(config.MaxCols, UInt(config.DATA_TYPE.W))))
     })
 
-    val matReg1 = RegInit(VecInit(Seq.fill(config.rowsA)(VecInit(Seq.fill(config.colsA)(0.U(config.DATA_TYPE.W))))))
-    val matReg2 = RegInit(VecInit(Seq.fill(config.rowsB)(VecInit(Seq.fill(config.colsB)(0.U(config.DATA_TYPE.W))))))
+    val matReg1 = RegInit(VecInit(Seq.fill(config.MaxRows)(VecInit(Seq.fill(config.MaxCols)(0.U(config.DATA_TYPE.W))))))
+    val matReg2 = RegInit(VecInit(Seq.fill(config.MaxRows)(VecInit(Seq.fill(config.MaxCols)(0.U(config.DATA_TYPE.W))))))
     io.bitmap1 := matReg1
     io.bitmap2 := matReg2
 
-    val i = RegInit(0.U(log2Ceil(config.rowsA).W))
-    val j = RegInit(0.U(log2Ceil(config.colsB).W))
+    val i = RegInit(0.U(log2Ceil(config.MaxRows).W))
+    val j = RegInit(0.U(log2Ceil(config.MaxCols).W))
 
-    when (j < config.colsB.U) {
+    when (j < config.MaxCols.U) {
                     when (io.mat1(i)(j) =/= 0.U){
                 matReg1(i)(j) := 1.U
             }.otherwise{
@@ -32,7 +32,7 @@ class Bitmap(implicit val config: MagmasiConfig) extends Module{
             }
          j := j + 1.U
 }
-  when (j === (config.colsB-1).U){
+  when (j === (config.MaxCols-1).U){
     i := i + 1.U
   }
 
