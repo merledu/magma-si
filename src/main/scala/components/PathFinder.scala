dc4f47ff5d9a8fa90e1bc0313d042b8d5f2e746e
masfiyan <asfiyanshivani0321@gmail.com>
2023-10-18 19:22:52 +0500

    PathFInder

---
+++ src/main/scala/components/PathFinder.scala
@@ -0,0 +1,31 @@
+package magmasi.components
+
+import chisel3._
+import chisel3.util._
+
+class PathFinder(implicit val config: MagmasiConfig) extends Module{
+
+    val io = IO(new Bundle{
+        val Stationary_matrix = Input(Vec(config.MaxRows, Vec(config.MaxCols, UInt(config.DATA_TYPE.W))))
+        val Streaming_matrix = Input(Vec(config.MaxRows, UInt(config.DATA_TYPE.W)))
+        val MuxBool = Output(Vec(2 * (config.LEVELS - 2) * config.NUM_PES + config.NUM_PES, Bool()))        
+        val Source  = Output(Vec(config.MaxRows * config.MaxCols, UInt(config.DATA_TYPE.W)))
+        val destination  = Output(Vec(config.MaxRows * config.MaxCols, UInt(config.DATA_TYPE.W)))
+    })
+
+    val myCounter = Module(new SourceDestination())
+    myCounter.io.Stationary_matrix := io.Stationary_matrix
+    myCounter.io.Streaming_matrix := io.Streaming_matrix
+
+    val myMuxes = Module(new Muxes())
+    myMuxes.io.mat1 := myCounter.io.counterMatrix1
+    myMuxes.io.mat2 := myCounter.io.counterMatrix2
+
+    val myWrapper = Module(new BenesWrapper())
+    myWrapper.io.i_mux_bus := myMuxes.io.i_mux_bus
+
+    io.MuxBool := myWrapper.io.o_mux_bus
+    io.Source := myMuxes.io.Source
+    io.destination := myMuxes.io.destination
+
+}
