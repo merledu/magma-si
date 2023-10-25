a27cfe5b9c4be2d44a4c25aac978c408ae003567
masfiyan <asfiyanshivani0321@gmail.com>
2023-10-25 21:56:53 +0500

    New SourceDestination unit TestCase

--- src/test/scala/components/SourceDestinationTester.scala
+++ src/test/scala/components/SourceDestinationTester.scala
@@ -9,25 +9,48 @@ class SourceDestinationTester extends AnyFreeSpec with ChiselScalatestTester {
     "Source Dest Table" in {
         implicit val config = MagmasiConfig()
         test(new SourceDestination()).withAnnotations(Seq(VerilatorBackendAnnotation)) { dut =>
-      val inputData = Seq(
-        Seq(0, 2, 3, 0),
-        Seq(5, 6, 7, 0),
-        Seq(9, 10, 11, 0),
-        Seq(13, 14, 15, 0)
-      )
-      val inputData2 = Seq(0,1,1,0,1,1,1,1,1)
+      
+        val inputData = Seq(
+          Seq(0, 2, 3, 0),
+          Seq(5, 6, 7, 0),
+          Seq(9, 10, 11, 0),
+          Seq(13, 14, 15, 0)
+        )
+        val inputData2 = Seq(0,1,1,0,1)
 
-      // Assign test values to KM_matrix
-      for (i <- 0 until inputData.length) {
-        for (j <- 0 until inputData(i).length) {
-          dut.io.Stationary_matrix(i)(j).poke(inputData(i)(j).U)
+        for (i <- 0 until inputData.length) {
+          for (j <- 0 until inputData(i).length) {
+            dut.io.Stationary_matrix(i)(j).poke(inputData(i)(j).U)
+          }
         }
-    }
+      
         for (i <- 0 until 4){
-            dut.io.Streaming_matrix(i).poke(inputData2(i).U)
+          dut.io.Streaming_matrix(i).poke(inputData2(i).U)
         }
-          dut.clock.step(65)
+      
+        dut.clock.step(65)
+      
+        val inputData3 = Seq(
+          Seq(0, 0, 0, 1),
+          Seq(0, 0, 0, 2),
+          Seq(0, 0, 0, 3),
+          Seq(0, 0, 0, 4)
+        )
+        val inputData4 = Seq(0,1,1,0,1,1,1,1,1)
+
+        for (i <- 0 until inputData.length) {
+          for (j <- 0 until inputData(i).length) {
+            dut.io.Stationary_matrix(i)(j).poke(inputData3(i)(j).U)
+          }
         }
+        
+        for (i <- 0 until 4){
+          dut.io.Streaming_matrix(i).poke(inputData4(i).U)
+        }
+        dut.clock.step(65)
+
+
+      }
     } 
 }
 
