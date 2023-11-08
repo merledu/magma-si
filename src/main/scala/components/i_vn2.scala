24feca81efb10d3296f7f9b1998443308b76d2c1
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2023-11-08 18:17:32 +0500

    Create i_vn2.scala

---
+++ src/main/scala/components/i_vn2.scala
@@ -0,0 +1,81 @@
+package magmasi.components
+
+import random
+
+import chisel3._
+import chisel3.util._
+
+class ivncontrol(DATA_TYPE: Int = 32, NUM_PES: Int = 32, LOG2_PES: Int = 5) extends Module {
+  val io = IO(new Bundle {
+ 
+
+    val Stationary_matrix = Input(Vec(config.MaxRows, Vec(config.MaxCols, UInt(config.DATA_TYPE.W))))
+
+    val o_vn = Output(Vec(NUM_PES, UInt(LOG2_PES.W)))
+
+  })
+
+val i_vn = RegInit(VecInit(Seq.fill(NUM_PES)(0.U(LOG2_PES.W))))
+val random_values = Seq.fill(NUM_PES)(Random.nextInt(32).U(LOG2_PES.W))
+val numRows = Stationary_matrix.length
+val numCols = Stationary_matrix(0).length
+val no_element = numRows * numCols
+var nonZeroCount = 0
+var noneachrow: Array[Int] = Array()
+
+for (i <- 0 until inputData.length)
+ {
+  for (j <- 0 until inputData(i).length) {
+    if (inputData(i)(j) != 0) {
+      nonZeroCount += 1
+    }
+  }
+}
+
+
+
+for (i <- 0 until inputData.length) {
+  var noneachrow = 0
+  for (j <- 0 until inputData(i).length) {
+    if (inputData(i)(j) != 0) {
+      noneachrow += 1
+    }
+  }
+  if (noneachrow > 0) {
+    noneachrow = noneachrow :+ noneachrow
+  }
+}
+
+for (i <- 0 until NUM_PES) {
+  i_vn(i) := random_values(i)
+}
+when(noneachrow(0) === 2 && (noneachrow(0)=== 2 || noneachrow(1) === 1 )){
+     
+      // FOR EN ADDER 0
+
+      i_vn(0) := "b10000".U
+      i_vn(1) := "b10000".U
+
+      
+    }.elsewhen(noneachrow(0) === 4 ){
+      // FOR ADDER 0,1,2 EN
+        i_vn(2) := "b10000".U
+        i_vn(3):= "b10000".U
+  
+      // FOR EN ADDER 0
+      i_vn(0) := "b10000".U
+      i_vn(1) := "b10000".U
+
+   
+
+    }.elsewhen(noneachrow(0) === 3){
+      //for adder 0 en
+    i_vn(0) := "b10000".U
+    i_vn(1) := "b10000"
+    //for adder 1 en
+    i_vn(1) := "b11000".U
+    i_vn(2) := "b11000".U
+    // for adder 2 cmd right select
+    i_vn(3) := "b11100".U
+    i_vn(4) := "b11110".U
+    }
