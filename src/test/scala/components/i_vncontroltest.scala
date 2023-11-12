611d1b3a5f77850393c0b059650b28fe06194be2
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2023-11-13 01:31:10 +0500

    Update i_vncontroltest.scala

--- src/test/scala/components/i_vncontroltest.scala
+++ src/test/scala/components/i_vncontroltest.scala
@@ -8,21 +8,29 @@ import org.scalatest.freespec.AnyFreeSpec
 class ivncontrolTester extends AnyFreeSpec with ChiselScalatestTester {
     "path finder tester" in {
         
-        test(new ivncontrol()).withAnnotations(Seq(VerilatorBackendAnnotation)) { dut =>
+        test(new ivncontrol(32,32,5,5,5)){ c =>
+          // println(s"noneachrow: ${c.noneachrow.mkString(", ")}")
+          // println(s"noneachrow(0): ${c.noneachrow(0)}")
+    
 
         val inputData = Seq(
-           Seq(1, 7, 2, 0),
-            Seq(3, 4, 1, 0),
-           Seq(2, 0, 4, 0),
-           Seq(1, 2, 3, 0)
+           Seq(1, 7, 2, 0,0),
+            Seq(3, 4, 1, 0,0),
+           Seq(2, 0, 4, 0,0),
+           Seq(1, 2, 3, 5,0),
+           Seq(1, 2, 3, 5,0)
+      
         ) 
      
-
-        for (i <- 0 until 4) {
-          for (j <- 0 until 4) {
-            dut.io.Stationary_matrix(i)(j).poke(inputData(i)(j).U)
+          println(s"noneachrow: ${c.noneachrow.mkString(", ")}")
+          println(s"noneachrow(0): ${c.noneachrow(0)}")
+        for (i <- 0 until 5) {
+          for (j <- 0 until 5) {
+            c.io.Stationary_matrix(i)(j).poke(inputData(i)(j).U)
           }
         }
+        c.clock.step(100)  
+    
     }
 }
 }
