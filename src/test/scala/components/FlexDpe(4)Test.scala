2abf89006911f0239078d4b613081de7d7b23726
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2023-11-25 01:05:25 +0500

    Update FlexDpe(4)Test.scala

--- src/test/scala/components/FlexDpe(4)Test.scala
+++ src/test/scala/components/FlexDpe(4)Test.scala
@@ -44,45 +44,43 @@ class flexdpecom4test extends AnyFreeSpec with ChiselScalatestTester {
             c.io.i_data_valid.poke(1.B)
 
         c.clock.step(100)
-        //    // for input data base 1
-        //     c.io.i_data_bus(0).poke(1.U)
-        //     c.io.i_data_bus(1).poke(1.U)
-        //     c.io.i_data_bus(2).poke(1.U)
-        //     c.io.i_data_bus(3).poke(1.U)
-        //   // for input data bus 2
-        //     c.io.i_data_bus2(0).poke(0.U)
-        //     c.io.i_data_bus2(1).poke(3.U)
-        //     c.io.i_data_bus2(2).poke(3.U)
-        //     c.io.i_data_bus2(3).poke(0.U)
 
-        //   // for i vn
-        //     c.io.i_vn(3).poke("b11101".U)
-        //     c.io.i_vn(4).poke("b11101".U)
+             // for input data base 1
+            c.io.i_data_bus(0).poke(1.U)
+            c.io.i_data_bus(1).poke(1.U)
+            c.io.i_data_bus(2).poke(1.U)
+            c.io.i_data_bus(3).poke(1.U)
+          // for input data bus 2
+            c.io.i_data_bus2(0).poke(3.U)
+            c.io.i_data_bus2(1).poke(3.U)
+            c.io.i_data_bus2(2).poke(3.U)
+            c.io.i_data_bus2(3).poke(3.U)
 
-        //     c.io.i_vn(7).poke("b10111".U)
-        //     c.io.i_vn(8).poke("b10111".U)
+              val inputData1 = Seq(
 
-        //     c.io.i_vn(11).poke("b01111".U)
-        //     c.io.i_vn(12).poke("b01111".U)
+           Seq(1, 1),
+           Seq(1, 1)      
+        ) 
 
-        //     c.io.i_vn(15).poke("b11110".U)
-        //     c.io.i_vn(16).poke("b11110".U)
-
-        //     c.io.i_vn(23).poke("b11111".U)
-        //     c.io.i_vn(24).poke("b11111".U)
-
-        //     c.io.i_vn(18).poke("b11111".U)
-        //     c.io.i_vn(13).poke("b11000".U)
-
-        //   // for muxes
-        //     c.io.i_mux_bus(576).poke(1.B)
-        //     c.io.i_mux_bus(579).poke(1.B)
-
-        //   // stationary and valid
-        //     c.io.i_stationary.poke(1.B)
-        //     c.io.i_data_valid.poke(1.B)
+        for (i <- 0 until 2) {
+          for (j <- 0 until 2) {
+            c.io.Stationary_matrix(i)(j).poke(inputData1(i)(j).U)
             
-        // c.clock.step(40)
+          }
+    
+    }
+
+
+          // for muxes
+            // c.io.i_mux_bus(0).poke(1.B)
+            // c.io.i_mux_bus(27).poke(1.B)
+
+          // stationary and valid
+            c.io.i_stationary.poke(1.B)
+            c.io.i_data_valid.poke(1.B)
+    
+            
+        c.clock.step(200)
     }
   }
 }
