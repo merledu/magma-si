843e711693785a2460cecacb387c8c35773d3f07
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2023-11-28 15:33:18 +0500

    Update i_vn(4)controllerTest.scala

--- src/test/scala/components/i_vn(4)controllerTest.scala
+++ src/test/scala/components/i_vn(4)controllerTest.scala
@@ -8,24 +8,27 @@ import org.scalatest.freespec.AnyFreeSpec
 
 class ivncontrol4Tester extends AnyFreeSpec with ChiselScalatestTester {
     "path finder tester" in {
-        
-        test(new ivncontrol4(32,4,5)){ c =>
+        implicit val Config = MagmasiConfig()
+        test(new ivncontrol4()){ c =>
     
 
         val inputData = Seq(
 
-           Seq(2, 4),
-           Seq(2, 0)      
+           Seq(1, 6,1),
+           Seq(0, 1,1)
+                 
         ) 
+        val numRows = inputData.length
+        val numCols = if (numRows > 0) inputData.head.length else 0
 
-        for (i <- 0 until 2) {
-          for (j <- 0 until 2) {
+        for (i <- 0 until numRows) {
+          for (j <- 0 until numCols) {
             c.io.Stationary_matrix(i)(j).poke(inputData(i)(j).U)
             
           }
     
     }
-      c.clock.step(30)
+      c.clock.step(100)
 }
 }
 }
