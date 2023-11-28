2c49423252a503b2757c09a99cec71406049c27b
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2023-11-29 02:29:08 +0500

    Update FlexDPE.scala

--- src/main/scala/components/FlexDPE.scala
+++ src/main/scala/components/FlexDPE.scala
@@ -3,18 +3,22 @@ package magmasi.components
 import chisel3._
 import chisel3.util._
 
-class flexdpecom2(IN_DATA_TYPE : Int = 32 ,DATA_TYPE: Int = 32, NUM_PES: Int = 32, LOG2_PES: Int = 5) extends Module {
+class flexdpecom4(IN_DATA_TYPE : Int = 32 ,DATA_TYPE: Int = 32, NUM_PES: Int = 4, LOG2_PES: Int = 5) extends Module {
     implicit val config = MagmasiConfig()
   val io = IO(new Bundle {
-    val i_vn = Input(Vec(NUM_PES, UInt(LOG2_PES.W)))
+    //val i_vn = Input(Vec(NUM_PES, UInt(LOG2_PES.W)))
     val i_stationary = Input(Bool())
     val i_data_valid = Input(Bool())
     val i_data_bus = Input(Vec(NUM_PES, UInt(DATA_TYPE.W)))
-     val i_data_bus2  = Input(Vec(NUM_PES, UInt(DATA_TYPE.W)))
+    val i_data_bus2  = Input(Vec(NUM_PES, UInt(DATA_TYPE.W)))
+
+    val Stationary_matrix = Input(Vec(2, Vec(2, UInt(DATA_TYPE.W))))
+     
+
 
     val o_valid = Output(Vec(NUM_PES, UInt(1.W)))
     val o_data_bus = Output(Vec(NUM_PES, UInt(DATA_TYPE.W)))
-     val o_adder = Output(Vec(NUM_PES, UInt(DATA_TYPE.W)))
+     val o_adder = Output(Vec(NUM_PES-1, UInt(DATA_TYPE.W)))
     val LEVELS   : Int = (2 * (math.log(NUM_PES) / math.log(2))).toInt + 1
      val i_mux_bus   = Input(Vec(2 * (LEVELS - 2) * NUM_PES + NUM_PES, Bool()))
   })
@@ -35,15 +39,17 @@ class flexdpecom2(IN_DATA_TYPE : Int = 32 ,DATA_TYPE: Int = 32, NUM_PES: Int = 3
      r_data_valid_ff2 := r_data_valid_ff
     
 
-    
-    val my_controller = Module(new fancontrol(32,32,5))
+    val my_ivn= Module(new ivncontrol4(32,4,5))
+    my_ivn.io.Stationary_matrix := io.Stationary_matrix
+    val i_vn = my_ivn.io.o_vn
 
-    my_controller.io.i_vn := io.i_vn
+    val my_controller = Module(new fancontrol4(32,4,5))
+
+    my_controller.io.i_vn := i_vn
     my_controller.io.i_stationary := 0.B
     my_controller.io.i_data_valid := io.i_data_valid
     val w_reduction_add = my_controller.io.o_reduction_add
     val w_reduction_cmd = my_controller.io.o_reduction_cmd
-    val w_reduction_sel = my_controller.io.o_reduction_sel
     val w_reduction_valid = my_controller.io.o_reduction_valid
 
     val my_Benes = Module(new Benes())
@@ -69,13 +75,13 @@ class flexdpecom2(IN_DATA_TYPE : Int = 32 ,DATA_TYPE: Int = 32, NUM_PES: Int = 3
 
 
 
-    val my_fan_network = Module(new FanNetworkcom(32,32))
+    val my_fan_network = Module(new Fan4(4,32))
 
     my_fan_network.io.i_valid := w_reduction_valid
     my_fan_network.io.i_data_bus :=   r_mult 
     my_fan_network.io.i_add_en_bus := w_reduction_add
     my_fan_network.io.i_cmd_bus := w_reduction_cmd
-    my_fan_network.io.i_sel_bus := w_reduction_sel
+
     io.o_valid := my_fan_network.io.o_valid
     io.o_data_bus := my_fan_network.io.o_data_bus
     io.o_adder :=  my_fan_network.io.o_adder
