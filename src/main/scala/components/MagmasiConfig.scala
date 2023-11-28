3bb2f9d26da75d7a11a46b28347958d7eef57f3c
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2023-11-29 02:32:49 +0500

    Update MagmasiConfig.scala

--- src/main/scala/components/MagmasiConfig.scala
+++ src/main/scala/components/MagmasiConfig.scala
@@ -5,12 +5,14 @@ case class MagmasiConfig (
     val DATA_TYPE: Int = 16,
     val NUM_PES  : Int = 4,
     val NUM_IN  : Int = 4,
-    val SEL_IN: Int = 32,
-    val NUM_OUT: Int =32,
+    val SEL_IN: Int = 2,
+    val NUM_OUT: Int =2,
     val MaxRows :Int = 4,
     val MaxCols :Int = 4,
     val LOG2_PES :Int = 5,
-    val NoOfFDPE :Int = 1 // 2 FlexDPE will be generated always
+    val NoOfFDPE :Int = 2
+
 ) {
     val LEVELS   : Int = (2 * (math.log(NUM_IN) / math.log(2))).toInt + 1
+    
 }
