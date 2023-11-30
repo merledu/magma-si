81d9f524a3f32ce0440ca3cf93e2e42591cc7a72
masfiyan <asfiyanshivani0321@gmail.com>
2023-12-01 00:16:59 +0500

    Update benes and delete extra stuff

--- src/main/scala/components/FlexDPU.scala
+++ src/main/scala/components/FlexDPU.scala
@@ -50,11 +50,16 @@ class FlexDPU(implicit val config:MagmasiConfig) extends Module{
 
 
     //------------------ Destination of flexdpe is complete ---------------------------------
-    val mux = WireInit(VecInit(Seq.fill(config.MaxRows * config.MaxCols)(0.U((config.LEVELS - 1).W))))
-    val src = WireInit(VecInit(Seq.fill(config.MaxRows * config.MaxCols)(0.U((config.LEVELS - 1).W))))
-    val dest = WireInit(VecInit(Seq.fill(config.MaxRows * config.MaxCols)(0.U((config.LEVELS - 1).W))))
-    val CountIteration = RegInit(0.U(32.W))
-    val PF_Stream_Col = RegInit(VecInit(Seq.fill(config.MaxRows)(0.U(32.W))))
+    val PF1mux = WireInit(VecInit(Seq.fill(config.MaxRows * config.MaxCols)(0.U((config.LEVELS - 1).W))))
+    val PF1src = WireInit(VecInit(Seq.fill(config.MaxRows * config.MaxCols)(0.U((config.LEVELS - 1).W))))
+    val PF1dest = WireInit(VecInit(Seq.fill(config.MaxRows * config.MaxCols)(0.U((config.LEVELS - 1).W))))
+    val PF2mux = WireInit(VecInit(Seq.fill(config.MaxRows * config.MaxCols)(0.U((config.LEVELS - 1).W))))
+    val PF2src = WireInit(VecInit(Seq.fill(config.MaxRows * config.MaxCols)(0.U((config.LEVELS - 1).W))))
+    val PF2dest = WireInit(VecInit(Seq.fill(config.MaxRows * config.MaxCols)(0.U((config.LEVELS - 1).W))))
+    val PF1CountIteration = RegInit(0.U(32.W))
+    val PF1_Stream_Col = RegInit(VecInit(Seq.fill(config.MaxRows)(0.U(32.W))))
+    val PF2CountIteration = RegInit(0.U(32.W))
+    val PF2_Stream_Col = RegInit(VecInit(Seq.fill(config.MaxRows)(0.U(32.W))))
 
     dontTouch(mux)
     dontTouch(src)
@@ -63,23 +68,56 @@ class FlexDPU(implicit val config:MagmasiConfig) extends Module{
         val PF1 = Module(new PathFinder)
         PF1.io.DataValid := 1.B
         PF1.io.Stationary_matrix := io.Stationary_matrix
-        PF1.io.NoDPE := 1.U // 0 means we need the src, muxes a/c to 1st DPE
-        PF1.io.Streaming_matrix := PF_Stream_Col
-        mux := PF1.io.i_mux_bus
-        src := PF1.io.Source
-        dest := PF1.io.destination
+        PF1.io.NoDPE := 0.U // 0 means we need the src, muxes a/c to 1st DPE
+        PF1.io.Streaming_matrix := PF1_Stream_Col
+        PF1mux := PF1.io.i_mux_bus
+        PF1src := PF1.io.Source
+        PF1dest := PF1.io.destination
 
         for (j <- 0 until config.MaxRows){
-            PF_Stream_Col(j) := io.Streaming_matrix(j)(CountIteration)
+            PF_Stream_Col(j) := io.Streaming_matrix(j)(PF1CountIteration)
         }
         
         when (PF1.io.PF_Valid){
-        CountIteration := CountIteration + 1.U
+        PF1CountIteration := PF1CountIteration + 1.U
         }
 
         when (CountIteration === (config.MaxCols-1).U){
-            CountIteration := CountIteration
+            PF1CountIteration := PF1CountIteration
+            val FDPE1 = Module(new flexdpecom4)
+            FDPE1.io.i_stationary := 1.B
+            FDPE1.io.i_data_valid := 1.B
+            FDPE1.io.
+
         } 
+
+        val PF2 = Module(new PathFinder)
+        PF2.io.DataValid := 1.B
+        PF2.io.Stationary_matrix := io.Stationary_matrix
+        PF2.io.NoDPE := 1.U // 0 means we need the src, muxes a/c to 1st DPE
+        PF2.io.Streaming_matrix := PF2_Stream_Col
+        mux := PF2.io.i_mux_bus
+        src := PF2.io.Source
+        dest := PF2.io.destination
+
+        for (j <- 0 until config.MaxRows){
+            PF2_Stream_Col(j) := io.Streaming_matrix(j)(PF2CountIteration)
+        }
+        
+        when (PF2.io.PF_Valid){
+        PF2CountIteration := PF2CountIteration + 1.U
+        }
+
+        when (PF2CountIteration === (config.MaxCols-1).U){
+            PF2CountIteration := PF2CountIteration
+        }
+
+
+
+
+
+
+
     }
     dontTouch(PF_Stream_Col)
     dontTouch(CountIteration)
