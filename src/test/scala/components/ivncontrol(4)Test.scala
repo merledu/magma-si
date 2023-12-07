b5659eb9ed12dfcb9d68aae17416e6216ca3e5c0
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2023-12-07 20:13:24 +0500

    Update ivncontrol(4)Test.scala

--- src/test/scala/components/ivncontrol(4)Test.scala
+++ src/test/scala/components/ivncontrol(4)Test.scala
@@ -1,40 +1,42 @@
 
-// package magmasi.components
+package magmasi.components
 
-// import chisel3._
-// import chisel3.util._
-// import chiseltest._ 
-// import org.scalatest.freespec.AnyFreeSpec
+import chisel3._
+import chisel3.util._
+import chiseltest._ 
+import org.scalatest.freespec.AnyFreeSpec
 
-// class ivncontrol4Tester extends AnyFreeSpec with ChiselScalatestTester {
-//     "path finder tester" in {
-//         implicit val Config = MagmasiConfig()
-//         test(new ivncontrol4()){ c =>
+class ivncontrol4Tester extends AnyFreeSpec with ChiselScalatestTester {
+    "ivn tester" in {
+        implicit val Config = MagmasiConfig()
+        test(new ivncontrol4()){ c =>
     
 
-//         val inputData = Seq(
+        val inputData = Seq(
 
-//              Seq(0, 0,0,0,0,0,0,0),
-//           Seq(4, 6,6,9,1,1,1,1),  
-//            Seq(0, 7,5,5,4,3,2,1),
-//           Seq(4, 6,6,9,1,1,1,1),        
-//            Seq(0, 7,5,5,4,3,2,1),
-//           Seq(4, 6,6,9,1,1,1,1),    
-//            Seq(0, 7,5,5,4,3,2,1),
-//           Seq(4, 6,6,9,1,1,1,0)    
-//         ) 
+          
+         Seq(711, 51,11,3,6,7,7,7),
+          Seq(7, 7,6,7,3,7,6,7),
+          Seq(7, 7,5,7,7,7,7,7),
+          Seq(7, 7,4,7,7,7,7,7),
+          Seq(7, 7,7,7,7,7,7,7),
+          Seq(4, 7,4,4,4,5,6,7),
+          Seq(6, 7,4,4,3,2,2,4),
+          Seq(2, 7,2,2,2,3,3,4)
 
-//         val numRows = inputData.length
-//         val numCols = if (numRows > 0) inputData.head.length else 0
+        ) 
 
-//         for (i <- 0 until numRows) {
-//           for (j <- 0 until numCols) {
-//             c.io.Stationary_matrix(i)(j).poke(inputData(i)(j).U)
+        val numRows = inputData.length
+        val numCols = if (numRows > 0) inputData.head.length else 0
+
+        for (i <- 0 until numRows) {
+          for (j <- 0 until numCols) {
+            c.io.Stationary_matrix(i)(j).poke(inputData(i)(j).U)
             
-//           }
+          }
     
-//     }
-//       c.clock.step(300)
-// }
-// }
-// }
+    }
+      c.clock.step(200)
+}
+}
+}
