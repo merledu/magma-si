7b906862a5eec65af0ac0d18388011b1b39065bf
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2023-12-09 18:37:05 +0500

    Create matrixOutTest.scala

---
+++ src/test/scala/components/matrixOutTest.scala
@@ -0,0 +1,52 @@
+
+package magmasi.components
+
+import chisel3._
+import chisel3.util._
+import chiseltest._ 
+import org.scalatest.freespec.AnyFreeSpec
+
+class MatrixOutTester extends AnyFreeSpec with ChiselScalatestTester {
+    "matrix tester" in {
+        implicit val Config = MagmasiConfig()
+        test(new MatrixOut()){ c =>
+    
+
+        val inputData = Seq(
+
+          
+         Seq(711, 51,11,3,6,7,7,7),
+          Seq(7, 7,6,7,3,7,6,7),
+          Seq(7, 7,5,7,7,7,7,7),
+          Seq(7, 7,4,7,7,7,7,7),
+          Seq(7, 7,7,7,7,7,7,7),
+          Seq(4, 7,4,4,4,5,6,7),
+          Seq(6, 7,4,4,3,2,2,4),
+          Seq(2, 7,2,2,2,3,3,4)
+
+        ) 
+
+        val numRows = inputData.length
+        val numCols = if (numRows > 0) inputData.head.length else 0
+
+        for (i <- 0 until numRows) {
+          for (j <- 0 until numCols) {
+            c.io.Stationary_matrix(i)(j).poke(inputData(i)(j).U)
+            
+          }
+        }
+        for (i <- 0 until numRows) {
+          for (j <- 0 until numCols) {
+            c.io.Streaming_matrix(i)(j).poke(inputData(i)(j).U)
+            
+          }
+    
+    }
+    c.io.i_stationary.poke(1.B)
+    c.io.CalFDE.poke(2.U)
+     c.io.i_data_valid.poke(1.B)
+
+      c.clock.step(200)
+}
+}
+}
