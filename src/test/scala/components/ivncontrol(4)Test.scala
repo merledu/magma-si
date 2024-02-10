b8cf0b11a54d3526702981743e0117ef08dcb8de
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2024-02-10 15:40:48 +0500

    Update ivncontrol(4)Test.scala

--- src/test/scala/components/ivncontrol(4)Test.scala
+++ src/test/scala/components/ivncontrol(4)Test.scala
@@ -1,36 +1,36 @@
 
-// package magmasi.components
+package magmasi.components
 
-// import chisel3._
-// import chisel3.util._
-// import chiseltest._ 
-// import org.scalatest.freespec.AnyFreeSpec
+import chisel3._
+import chisel3.util._
+import chiseltest._ 
+import org.scalatest.freespec.AnyFreeSpec
 
-// class ivncontrol4Tester extends AnyFreeSpec with ChiselScalatestTester {
-//     "ivn tester" in {
-//         implicit val Config = MagmasiConfig()
-//         test(new ivncontrol4()){ c =>
+class ivncontrol4Tester extends AnyFreeSpec with ChiselScalatestTester {
+    "ivn tester" in {
+        implicit val Config = MagmasiConfig()
+        test(new ivncontrol4()){ c =>
     
 
-//         val inputData = Seq(
+        val inputData = Seq(
 
           
-//       Seq(2,2),
-//       Seq(2,2)
+      Seq(2,2),
+      Seq(2,2)
 
-//         ) 
+        ) 
 
-//         val numRows = inputData.length
-//         val numCols = if (numRows > 0) inputData.head.length else 0
+        val numRows = inputData.length
+        val numCols = if (numRows > 0) inputData.head.length else 0
 
-//         for (i <- 0 until numRows) {
-//           for (j <- 0 until numCols) {
-//             c.io.Stationary_matrix(i)(j).poke(inputData(i)(j).U)
+        for (i <- 0 until numRows) {
+          for (j <- 0 until numCols) {
+            c.io.Stationary_matrix(i)(j).poke(inputData(i)(j).U)
             
-//           }
+          }
     
-//     }
-//       c.clock.step(200)
-// }
-// }
-// }
+    }
+      c.clock.step(200)
+}
+}
+}
