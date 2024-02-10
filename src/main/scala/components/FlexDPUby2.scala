b76e9e69bb40954fa5aa6bd2ccc7d5308699fc71
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2024-02-10 15:10:58 +0500

    Update FlexDPUby2.scala

--- src/main/scala/components/FlexDPUby2.scala
+++ src/main/scala/components/FlexDPUby2.scala
@@ -77,7 +77,7 @@ class FlexDPUby2(implicit val config:MagmasiConfig) extends Module{
     }
 
 
-    // ------------------------- source destination calculation is complete ---------------------
+    // ------------------------- source destination calculation is complete ----------------------
     
     def Reverse(in:UInt):UInt={
         val rev =(0 until 4).foldLeft(0.U) { (acc, i) =>
