dcb9e528c0bffc23aeb9b06fb3748992ec8284ce
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2024-02-10 15:37:20 +0500

    Update BitmapTester.scala

--- src/test/scala/components/BitmapTester.scala
+++ src/test/scala/components/BitmapTester.scala
@@ -1,30 +1,30 @@
-// package magmasi.components
+package magmasi.components
 
-// import chisel3._
-// import chisel3.util._
-// import chiseltest._ 
-// import org.scalatest.freespec.AnyFreeSpec
+import chisel3._
+import chisel3.util._
+import chiseltest._ 
+import org.scalatest.freespec.AnyFreeSpec
 
-// class BitmapTester extends AnyFreeSpec with ChiselScalatestTester {
-//     "Bitmap Table" in {
-//         implicit val config = MagmasiConfig()
-//         test(new Bitmap()).withAnnotations  (Seq(VerilatorBackendAnnotation)) { dut =>
-//       val inputData = Seq(
-//         Seq(0, 2, 3, 0),
-//         Seq(5, 6, 7, 0),
-//         Seq(9, 10, 11, 0),
-//         Seq(13, 14, 15, 0)
-//       ) 
+class BitmapTester extends AnyFreeSpec with ChiselScalatestTester {
+    "Bitmap Table" in {
+        implicit val config = MagmasiConfig()
+        test(new Bitmap()).withAnnotations  (Seq(VerilatorBackendAnnotation)) { dut =>
+      val inputData = Seq(
+        Seq(0, 2, 3, 0),
+        Seq(5, 6, 7, 0),
+        Seq(9, 10, 11, 0),
+        Seq(13, 14, 15, 0)
+      ) 
 
 
-//       // Assign test values to KM_matrix
-//       for (i <- 0 until 4) {
-//         for (j <- 0 until 4) {
-//           dut.io.mat1(i)(j).poke(inputData(i)(j).U)
-//           dut.io.mat2(i)(j).poke(inputData(i)(j).U)
-//         }
-//     }
-//           dut.clock.step(65)
-//         }
-//     } 
-// }
+      // Assign test values to KM_matrix
+      for (i <- 0 until 4) {
+        for (j <- 0 until 4) {
+          dut.io.mat1(i)(j).poke(inputData(i)(j).U)
+          dut.io.mat2(i)(j).poke(inputData(i)(j).U)
+        }
+    }
+          dut.clock.step(65)
+        }
+    } 
+}
