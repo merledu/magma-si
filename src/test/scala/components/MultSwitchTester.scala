2325f8fa294b350ffff21863a17bd60e5ce78eb1
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2024-02-10 15:36:17 +0500

    Update MultSwitchTester.scala

--- src/test/scala/components/MultSwitchTester.scala
+++ src/test/scala/components/MultSwitchTester.scala
@@ -1,21 +1,21 @@
 
-package magmasi.components
+// package magmasi.components
 
-import chisel3._
-import chisel3.util._
-import chiseltest._ 
-import org.scalatest.freespec.AnyFreeSpec
+// import chisel3._
+// import chisel3.util._
+// import chiseltest._ 
+// import org.scalatest.freespec.AnyFreeSpec
 
-class MultSwitchTester extends AnyFreeSpec with ChiselScalatestTester {
-    "multiplier Switch Test" in {
-        test(new MultSwitch()){ dut =>
-            dut.io.i_valid.poke(1.B)
-            dut.io.i_data.poke(2.U)
-            dut.io.i_stationary.poke(1.U)
-            dut.io.o_valid.expect(0.B)
-            dut.io.o_data.expect(0.U)
+// class MultSwitchTester extends AnyFreeSpec with ChiselScalatestTester {
+//     "multiplier Switch Test" in {
+//         test(new MultSwitch()){ dut =>
+//             dut.io.i_valid.poke(1.B)
+//             dut.io.i_data.poke(2.U)
+//             dut.io.i_stationary.poke(1.U)
+//             dut.io.o_valid.expect(0.B)
+//             dut.io.o_data.expect(0.U)
 
-            dut.clock.step(2)
-        }
-    }
-}
+//             dut.clock.step(2)
+//         }
+//     }
+// }
