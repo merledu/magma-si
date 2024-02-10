421fc24cb756562136d7d1859bbbbdd485f3074c
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2024-02-10 15:35:08 +0500

    Update TopTester.scala

--- src/test/scala/components/TopTester.scala
+++ src/test/scala/components/TopTester.scala
@@ -1,37 +1,37 @@
-package magmasi.components
+// package magmasi.components
 
-import chisel3._
-import chisel3.util._
-import chiseltest._ 
-import org.scalatest.freespec.AnyFreeSpec
+// import chisel3._
+// import chisel3.util._
+// import chiseltest._ 
+// import org.scalatest.freespec.AnyFreeSpec
 
-class TopTester extends AnyFreeSpec with ChiselScalatestTester {
-    "FlexDPU tester" in {
-        implicit val config = MagmasiConfig()
-        test(new Top()){ c =>
-         val inputData = Seq(
-          Seq(1,1),
-          Seq(1, 1),
+// class TopTester extends AnyFreeSpec with ChiselScalatestTester {
+//     "FlexDPU tester" in {
+//         implicit val config = MagmasiConfig()
+//         test(new Top()){ c =>
+//          val inputData = Seq(
+//           Seq(1,1),
+//           Seq(1, 1),
         
-        )
-        val inputData2 = Seq(
-           Seq(1,3),
-            Seq(2,1),
+//         )
+//         val inputData2 = Seq(
+//            Seq(1,3),
+//             Seq(2,1),
        
-        ) 
-        for ( i <- 0 until 2){
-          for ( j <- 0 until 2){
-              c.io.Streaming_matrix(i)(j).poke(inputData2(i)(j).U)
-          }
-        }
-        for ( i <- 0 until config.MaxRows){
-          for ( j <- 0 until config.MaxCols){
-              c.io.Stationary_matrix(i)(j).poke(inputData(i)(j).U)
-          }
-        }
-        c.clock.step(400)
-    }
-  }
-}
+//         ) 
+//         for ( i <- 0 until 2){
+//           for ( j <- 0 until 2){
+//               c.io.Streaming_matrix(i)(j).poke(inputData2(i)(j).U)
+//           }
+//         }
+//         for ( i <- 0 until config.MaxRows){
+//           for ( j <- 0 until config.MaxCols){
+//               c.io.Stationary_matrix(i)(j).poke(inputData(i)(j).U)
+//           }
+//         }
+//         c.clock.step(400)
+//     }
+//   }
+// }
 
 
