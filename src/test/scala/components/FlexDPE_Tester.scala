7cb073f089d34b92c168fcc7678d394167796304
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2024-02-10 15:33:01 +0500

    Update FlexDPE_Tester.scala

--- src/test/scala/components/FlexDPE_Tester.scala
+++ src/test/scala/components/FlexDPE_Tester.scala
@@ -1,38 +1,38 @@
-package magmasi.components
+// package magmasi.components
 
-import chisel3._
-import chisel3.util._
-import chiseltest._ 
-import org.scalatest.freespec.AnyFreeSpec
+// import chisel3._
+// import chisel3.util._
+// import chiseltest._ 
+// import org.scalatest.freespec.AnyFreeSpec
 
-class FlexDPETester extends AnyFreeSpec with ChiselScalatestTester {
-    "FlexDPU tester" in {
-        implicit val config = MagmasiConfig()
-        test(new FlexDPUby2()).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
-        c.io.valid.poke(1.B)
-        val inputData = Seq(
-          Seq(0,1),
-          Seq(1, 1),
+// class FlexDPETester extends AnyFreeSpec with ChiselScalatestTester {
+//     "FlexDPU tester" in {
+//         implicit val config = MagmasiConfig()
+//         test(new FlexDPUby2()).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
+//         c.io.valid.poke(1.B)
+//         val inputData = Seq(
+//           Seq(0,1),
+//           Seq(1, 1),
         
-        )
-        val inputData2 = Seq(
-           Seq(1,3),
-            Seq(2,1),
+//         )
+//         val inputData2 = Seq(
+//            Seq(1,3),
+//             Seq(2,1),
        
-        ) 
-        for ( i <- 0 until config.MaxRows){
-          for ( j <- 0 until config.MaxCols){
-              c.io.Streaming_matrix(i)(j).poke(inputData2(i)(j).U)
-          }
-        }
-        for ( i <- 0 until config.MaxRows){
-          for ( j <- 0 until config.MaxCols){
-              c.io.Stationary_matrix(i)(j).poke(inputData(i)(j).U)
-          }
-        }
-        //c.io.CalFDE.poke(2.U)
-        c.clock.step(400)
-    }
-  }
-}
+//         ) 
+//         for ( i <- 0 until config.MaxRows){
+//           for ( j <- 0 until config.MaxCols){
+//               c.io.Streaming_matrix(i)(j).poke(inputData2(i)(j).U)
+//           }
+//         }
+//         for ( i <- 0 until config.MaxRows){
+//           for ( j <- 0 until config.MaxCols){
+//               c.io.Stationary_matrix(i)(j).poke(inputData(i)(j).U)
+//           }
+//         }
+//         //c.io.CalFDE.poke(2.U)
+//         c.clock.step(400)
+//     }
+//   }
+// }
 
