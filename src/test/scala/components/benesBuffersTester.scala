44f07c95f1da9f7ad13098ce5b53240fa526fbce
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2024-02-10 15:40:29 +0500

    Update benesBuffersTester.scala

--- src/test/scala/components/benesBuffersTester.scala
+++ src/test/scala/components/benesBuffersTester.scala
@@ -1,39 +1,39 @@
 
-// package magmasi.components
+package magmasi.components
 
-// import chisel3._
-// import chisel3.util._
-// import chiseltest._ 
-// import org.scalatest.freespec.AnyFreeSpec
+import chisel3._
+import chisel3.util._
+import chiseltest._ 
+import org.scalatest.freespec.AnyFreeSpec
 
-// class benesBuffersTester extends AnyFreeSpec with ChiselScalatestTester {
-//     "My Benes Top test" in {
-//         implicit val config = MagmasiConfig()
-//         test(new benseBuffers()).withAnnotations(Seq(VerilatorBackendAnnotation)) { dut =>
+class benesBuffersTester extends AnyFreeSpec with ChiselScalatestTester {
+    "My Benes Top test" in {
+        implicit val config = MagmasiConfig()
+        test(new benseBuffers()).withAnnotations(Seq(VerilatorBackendAnnotation)) { dut =>
 
             
-//             val inputData = Seq(1, 2, 3, 4, 5, 6, 7, 8)
+            val inputData = Seq(1, 2, 3, 4, 5, 6, 7, 8)
 
-//       // Connect input data to the DUT
-//       for (i <- 0 until 4) {
-//         dut.io.i_data_bus1(i).poke(inputData(i).U)
-//       }
-
-
-//         dut.io.i_mux_bus(0).poke(3.U)
-//         dut.io.i_mux_bus(1).poke(3.U)
-//         dut.io.i_mux_bus(2).poke(3.U)
-//         dut.io.i_mux_bus(3).poke(3.U)
-//         val LEVELS   : Int = (2 * (math.log(4) / math.log(2))).toInt + 1
+      // Connect input data to the DUT
+      for (i <- 0 until 4) {
+        dut.io.i_data_bus1(i).poke(inputData(i).U)
+      }
 
 
+        dut.io.i_mux_bus(0).poke(3.U)
+        dut.io.i_mux_bus(1).poke(3.U)
+        dut.io.i_mux_bus(2).poke(3.U)
+        dut.io.i_mux_bus(3).poke(3.U)
+        val LEVELS   : Int = (2 * (math.log(4) / math.log(2))).toInt + 1
 
 
 
-//       dut.clock.step(3)
+
+
+      dut.clock.step(3)
 
 
 
-//     }
-//   }
-// }
+    }
+  }
+}
