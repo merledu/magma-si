3f7af05f34e511656ac39f0da06a1826435d1dab
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2024-02-10 15:39:39 +0500

    Update RegorTester.scala

--- src/test/scala/components/RegorTester.scala
+++ src/test/scala/components/RegorTester.scala
@@ -1,37 +1,35 @@
-// package magmasi.components
+package magmasi.components
 
-// import chisel3._
-// import chisel3.util._
-// import chiseltest._ 
-// import org.scalatest.freespec.AnyFreeSpec
+import chisel3._
+import chisel3.util._
+import chiseltest._ 
+import org.scalatest.freespec.AnyFreeSpec
 
-// class RegorTester extends AnyFreeSpec with ChiselScalatestTester {
-//     "Temporary Register step 3 Table" in {
-//         implicit val config = MagmasiConfig()
-//         test(new Regor(2,2,32)).withAnnotations  (Seq(VerilatorBackendAnnotation)) { dut =>
-//       val inputData = Seq(
-//         Seq(0, 1, 3, 1),
-//         Seq(0, 0, 7, 0),
-//         Seq(9, 10, 11, 0),
-//         Seq(1, 2, 3, 0)
-//       ) 
-//         val inputData1 = Seq(
-//         Seq(1, 2, 0, 0),
-//         Seq(5, 6, 7, 0),
-//         Seq(9, 0, 0, 11),
-//         Seq(0, 0, 0, 0)
-//       )
+class RegorTester extends AnyFreeSpec with ChiselScalatestTester {
+    "Temporary Register step 3 Table" in {
+        implicit val config = MagmasiConfig()
+        test(new Regor(2,2,32)).withAnnotations  (Seq(VerilatorBackendAnnotation)) { dut =>
+      val inputData = Seq(
+        Seq(0, 1),
+        Seq(0, 0)
+     
+      ) 
+        val inputData1 = Seq(
+        Seq(1, 2),
+        Seq(5, 6)
+     
+      )
 
 
-//       // Assign test values to KM_matrix
-//       for (i <- 0 until 2) {
-//         for (j <- 0 until 2) {
-//           dut.io.mat1(i)(j).poke(inputData(i)(j).U)
-//           dut.io.mat2(i)(j).poke(inputData1(i)(j).U)
-//         }
-//     }
-//           dut.clock.step(65)
-//         }
-//     } 
-// }
+      // Assign test values to KM_matrix
+      for (i <- 0 until 2) {
+        for (j <- 0 until 2) {
+          dut.io.mat1(i)(j).poke(inputData(i)(j).U)
+          dut.io.mat2(i)(j).poke(inputData1(i)(j).U)
+        }
+    }
+          dut.clock.step(65)
+        }
+    } 
+}
 
